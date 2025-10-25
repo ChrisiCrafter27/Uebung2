1c1ccdea50efe6cc787c9f6a520137b9327507f6
ChrisiCrafter27 <c.schwarz27@web.de>
2025-10-25 14:02:15 +0200

    Added H3_main.java

---
+++ src/h3/H3_main.java
@@ -0,0 +1,24 @@
+package h3;
+
+import java.util.Random;
+
+public class H3_main {
+    public static void main(String[] args) {
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        boolean b1 = i > j;
+        boolean b2 = i > 200;
+        boolean b3 = j > 100;
+
+        k = -10;
+
+        if(b1) {
+            if(!b3) {
+                if(!b2) k = 1;
+                else k = 2;
+            } else if(b2) k = 3;
+        } else if(!b2 && !b3) k = 4;
+    }
+}
